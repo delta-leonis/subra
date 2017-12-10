ade840c675e070adaf49f93b8a61eff5ec5ec39c
Jeroen de Jong <thumbnail95@gmail.com>
2017-12-10 22:33:08 +0100

    Refactor PotentialBackground to accept GaussianPotentialFields

---
+++ src/main/java/io/leonis/subra/gui/field/GaussianPotentialFieldBackground.java
@@ -0,0 +1,24 @@
+package io.leonis.subra.gui.field;
+
+import com.googlecode.lanterna.TextColor;
+import io.leonis.subra.math.spatial.GaussianPotentialField;
+import java.util.function.*;
+import lombok.AllArgsConstructor;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
+
+/**
+ * @author Jeroen de Jong
+ */
+@AllArgsConstructor
+public class GaussianPotentialFieldBackground implements BiFunction<Integer, Integer, TextColor> {
+  private final GaussianPotentialField potentialField;
+  private final Function<Double, TextColor.RGB> gradient;
+  private final static double LIMIT = 100;
+
+  @Override
+  public TextColor apply(final Integer x, final Integer y) {
+    final INDArray positionVector = potentialField.getPotential(Nd4j.create(new double[]{x, y}));
+    return gradient.apply(positionVector.getDouble(0, 0) / LIMIT);
+  }
+}
