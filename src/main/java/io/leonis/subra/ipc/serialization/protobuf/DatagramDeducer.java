791d649bc2261a711d8b189a6252810c826b665d
romnous <rimooz@gmail.com>
2018-01-17 15:15:20 +0100

    fix generic parameter to DatagramDeducer (#19)

--- src/main/java/io/leonis/subra/ipc/serialization/protobuf/DatagramDeducer.java
+++ src/main/java/io/leonis/subra/ipc/serialization/protobuf/DatagramDeducer.java
@@ -11,12 +11,13 @@ import reactor.core.publisher.Flux;
  *
  * Deduces a protobuf {@link Message} to a {@link DatagramPacket}.
  *
+ * @param <M> The type of {@link Message} to convert to a {@link DatagramPacket}.
  * @author Jeroen de Jong
  */
-public class DatagramDeducer implements Deducer<Message, DatagramPacket> {
+public class DatagramDeducer<M extends Message> implements Deducer<M, DatagramPacket> {
 
   @Override
-  public Publisher<DatagramPacket> apply(final Publisher<Message> messagePublisher) {
+  public Publisher<DatagramPacket> apply(final Publisher<M> messagePublisher) {
     return Flux.from(messagePublisher)
         .map(message -> new DatagramPacket(message.toByteArray(), message.getSerializedSize()));
   }
