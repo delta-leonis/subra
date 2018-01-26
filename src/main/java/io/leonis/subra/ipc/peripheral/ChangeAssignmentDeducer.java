4c50ac81f272f1d75f0e22d7313e8a81e485e0eb
Jeroen de Jong <thumbnail95@gmail.com>
2018-01-26 20:21:29 +0100

    Add @romnious snippet for changing selection

--- src/main/java/io/leonis/subra/ipc/peripheral/ChangeAssignmentDeducer.java
+++ src/main/java/io/leonis/subra/ipc/peripheral/ChangeAssignmentDeducer.java
@@ -1,7 +1,6 @@
 package io.leonis.subra.ipc.peripheral;
 
-import static java.util.stream.Collectors.*;
-
+import com.google.common.collect.ImmutableSet;
 import io.leonis.subra.ipc.peripheral.ChangeAssignmentDeducer.Frame;
 import io.leonis.zosma.game.Identity;
 import io.leonis.zosma.game.engine.Deducer;
@@ -9,13 +8,21 @@ import io.leonis.zosma.ipc.peripheral.Controller;
 import io.leonis.zosma.ipc.peripheral.Controller.ControllerIdentity;
 import io.leonis.zosma.ipc.peripheral.component.Dpad;
 import java.util.AbstractMap.SimpleImmutableEntry;
-import java.util.*;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
+import java.util.TreeSet;
 import java.util.function.BiFunction;
-import java.util.stream.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import lombok.Value;
 import org.reactivestreams.Publisher;
 import reactor.core.publisher.Flux;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.mapping;
+import static java.util.stream.Collectors.toSet;
 
 /**
  * @author jeroen.dejong.
@@ -29,11 +36,36 @@ implements Deducer<T, Frame<I>> {
 
   private Comparator<I> comparator;
 
-  private BiFunction<Map<I, Set<ControllerIdentity>>, C, Map<I, Set<ControllerIdentity>>> f = (a, b) -> {
-    List<Entry<I, Set<ControllerIdentity>>> metsj = a.entrySet().stream()
-        .sorted((entry, entry2)-> comparator.compare(entry.getKey(), entry2.getKey())).collect(toList());
-    return Collections.emptyMap();
-  };
+  private BiFunction<Map<I, Set<ControllerIdentity>>, C, Map<I, Set<ControllerIdentity>>> f =
+      (controllerMapping, controller) -> {
+        final TreeSet<I> ids = controllerMapping.entrySet().stream()
+            .map(Entry::getKey)
+            .collect(Collectors.toCollection(() -> new TreeSet<>(comparator)));
+
+        return controllerMapping.entrySet().stream()
+            .collect(Collectors.toMap(
+                Entry::getKey,
+                entry -> {
+                  if (controller.getDpad().isLeft()
+                      && controllerMapping.get(ids.higher(entry.getKey()))
+                      .contains(controller.getIdentity())
+                      || (controller.getDpad().isRight()
+                      && controllerMapping.get(ids.lower(entry.getKey()))
+                      .contains(controller.getIdentity()))
+                      ) {
+                    return ImmutableSet.<ControllerIdentity>builder()
+                        .addAll(entry.getValue())
+                        .add(controller.getIdentity())
+                        .build();
+                  }
+                  else if (controller.getDpad().isLeft() || controller.getDpad().isRight()) {
+                    return entry.getValue().stream()
+                        .filter(a -> a.equals(controller.getIdentity()))
+                        .collect(Collectors.toSet());
+                  }
+                  return entry.getValue();
+                }));
+      };
 
   @Override
   public Publisher<Frame<I>> apply(final Publisher<T> tPublisher) {
