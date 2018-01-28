ae8ffabe4a8190a16346b5a098492b344900577e
Jeroen de Jong <thumbnail95@gmail.com>
2018-01-28 17:50:17 +0100

    WIP New take on ChangeAssignmentDeducer

--- src/main/java/io/leonis/subra/examples/ControllerExample.java
+++ src/main/java/io/leonis/subra/examples/ControllerExample.java
@@ -1,46 +1,40 @@
 package io.leonis.subra.examples;
 
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableSet;
-import io.leonis.subra.game.data.Player;
-import io.leonis.subra.game.data.TeamColor;
+import com.google.common.collect.*;
+import io.leonis.subra.game.data.*;
+import io.leonis.subra.game.data.Player.PlayerIdentity;
 import io.leonis.subra.ipc.peripheral.ChangeAssignmentDeducer;
-import io.leonis.subra.ipc.peripheral.ControllerHandler;
-import io.leonis.subra.math.AveragePlayerCommand;
-import io.leonis.zosma.ipc.peripheral.Controller;
-import io.leonis.zosma.ipc.peripheral.ControllerDeducer;
-import io.leonis.zosma.ipc.peripheral.ControllerPublisher;
-import io.leonis.zosma.ipc.peripheral.XboxController;
-import io.leonis.zosma.ipc.peripheral.XboxJamepadAdapter;
+import io.leonis.zosma.ipc.peripheral.Controller.ControllerIdentity;
+import io.leonis.zosma.ipc.peripheral.*;
 import java.time.Duration;
 import java.util.*;
 import reactor.core.publisher.Flux;
 
 /**
- * @author Jeroen de Jong
- * @date 26-1-2018
+ * @author jeroen.dejong.
  */
 public class ControllerExample {
 
-    public static void main(String[] args) {
-        Flux.from(
-            new ControllerPublisher<>(6, Duration.ofMillis(100), new XboxJamepadAdapter(),
-                ImmutableMap.of(
-                    new Player.PlayerIdentity(1, TeamColor.BLUE), ImmutableSet.of(new Controller.ControllerIdentity(0)),
-                    new Player.PlayerIdentity(2, TeamColor.BLUE), Collections.emptySet(),
-                    new Player.PlayerIdentity(3, TeamColor.BLUE), Collections.emptySet(),
-                    new Player.PlayerIdentity(4, TeamColor.BLUE), Collections.emptySet())))
-            .transform(new ChangeAssignmentDeducer<>(Comparator.comparingInt(Player.PlayerIdentity::getId)))
-            .map(Controller.MapSupplier::getControllerMapping)
-            .subscribe(m -> m.forEach((key, value) -> {
-                System.out.print(key.getId());
-                System.out.print(" => ");
-                value.stream().forEach(bal -> {
-                  System.out.print("controller: " + bal.getIndex() + ", ");
-                });
-              System.out.println();
-            }));
+  public static void main(final String[] args) {
+    final Map<PlayerIdentity, Set<ControllerIdentity>> controllerMapping = ImmutableMap.of(
+        new PlayerIdentity(1, TeamColor.BLUE), ImmutableSet.of(new ControllerIdentity(0)),
+        new PlayerIdentity(2, TeamColor.BLUE), Collections.emptySet(),
+        new PlayerIdentity(3, TeamColor.BLUE), Collections.emptySet(),
+        new PlayerIdentity(4, TeamColor.BLUE), Collections.emptySet());
 
-        while(true) {}
-    }
+    Flux.from(new ControllerSetPublisher<>(2, Duration.ofMillis(100), new XboxJamepadAdapter()))
+        .transform(new ChangeAssignmentDeducer<>(Flux.just(() -> controllerMapping),
+            Comparator.comparingInt(Player.PlayerIdentity::getId)))
+        .subscribe(m -> m.getControllerMapping().forEach((key, value) -> {
+              System.out.print(key.getId());
+              System.out.print(" => ");
+              value.forEach(bal -> {
+                System.out.print("controller: " + bal.getIndex() + ", ");
+              });
+              System.out.println();
+            })
+        );
+
+    while(true) {}
+  }
 }
--- src/main/java/io/leonis/subra/ipc/peripheral/ChangeAssignmentDeducer.java
+++ src/main/java/io/leonis/subra/ipc/peripheral/ChangeAssignmentDeducer.java
@@ -5,19 +5,17 @@ import io.leonis.subra.ipc.peripheral.ChangeAssignmentDeducer.Frame;
 import io.leonis.zosma.game.Identity;
 import io.leonis.zosma.game.engine.Deducer;
 import io.leonis.zosma.ipc.peripheral.Controller;
-import io.leonis.zosma.ipc.peripheral.Controller.ControllerIdentity;
+import io.leonis.zosma.ipc.peripheral.Controller.*;
 import io.leonis.zosma.ipc.peripheral.component.Dpad;
+import java.util.*;
 import java.util.AbstractMap.SimpleImmutableEntry;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.BiFunction;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import lombok.Value;
+import lombok.experimental.Delegate;
 import org.reactivestreams.Publisher;
 import reactor.core.publisher.Flux;
 import static java.util.stream.Collectors.groupingBy;
@@ -26,63 +24,81 @@ import static java.util.stream.Collectors.toSet;
 
 /**
  * @author jeroen.dejong.
+ * todo This class should probably not return a Controller.SetSupplier, just to reduce it's responsibility.
  */
 @Value
 public class ChangeAssignmentDeducer<
     I extends Identity,
     C extends Controller & Dpad.Supplier,
-    T extends Controller.MapSupplier<I> & Controller.SetSupplier<C>>
-implements Deducer<T, Frame<I>> {
+    M extends Controller.MapSupplier<I>,
+    S extends Controller.SetSupplier<C>>
+implements Deducer<S, Frame<C, I>> {
 
-  private Comparator<I> comparator;
+  private BiFunction<Map<I, Set<ControllerIdentity>>, C, Map<I, Set<ControllerIdentity>>> combiner;
 
-  private BiFunction<Map<I, Set<ControllerIdentity>>, C, Map<I, Set<ControllerIdentity>>> combiner =
-      (controllerMapping, controller) -> {
-        final TreeSet<I> ids = controllerMapping.entrySet().stream()
-            .map(Entry::getKey)
-            .collect(Collectors.toCollection(() -> new TreeSet<>(comparator)));
+  private final AtomicReference<Map<I, Set<ControllerIdentity>>> currentMapping =
+      new AtomicReference<>(Collections.emptyMap());
 
-        return controllerMapping.entrySet().stream()
-            .collect(Collectors.toMap(
-                Entry::getKey,
-                entry -> {
-                  if (controller.getDpad().isLeft()
-                      && controllerMapping.get(ids.higher(entry.getKey()))
-                      .contains(controller.getIdentity())
-                      || (controller.getDpad().isRight()
-                      && controllerMapping.get(ids.lower(entry.getKey()))
-                      .contains(controller.getIdentity()))
-                      ) {
-                    return ImmutableSet.<ControllerIdentity>builder()
-                        .addAll(entry.getValue())
-                        .add(controller.getIdentity())
-                        .build();
-                  }
-                  else if (controller.getDpad().isLeft() || controller.getDpad().isRight()) {
-                    return entry.getValue().stream()
-                        .filter(a -> a.equals(controller.getIdentity()))
-                        .collect(Collectors.toSet());
-                  }
-                  return entry.getValue();
-                }));
-      };
+  public ChangeAssignmentDeducer(final Publisher<M> mapSource, final Comparator<I> comparator) {
+    // Override current mapping with whatever comes through the publisher.
+    Flux.from(mapSource)
+        .map(MapSupplier::getControllerMapping)
+        .subscribe(currentMapping::set);
+
+    // Build the combiner based on the comparator in the constructor
+    // TODO this should probably become a seperate function on it's own.
+    this.combiner = (controllerMapping, controller) -> {
+      final TreeSet<I> ids = controllerMapping.entrySet().stream()
+          .map(Entry::getKey)
+          .collect(Collectors.toCollection(() -> new TreeSet<>(comparator)));
+
+      return controllerMapping.entrySet().stream()
+          .collect(Collectors.toMap(
+              Entry::getKey,
+              entry -> {
+                if (controller.getDpad().isRight()
+                    && controllerMapping.containsKey(ids.higher(entry.getKey()))
+                    && controllerMapping.get(ids.higher(entry.getKey()))
+                    .contains(controller.getIdentity())
+                    || (controller.getDpad().isLeft()
+                    && controllerMapping.containsKey(ids.lower(entry.getKey()))
+                    && controllerMapping.get(ids.lower(entry.getKey()))
+                    .contains(controller.getIdentity()))
+                    ) {
+                  return ImmutableSet.<ControllerIdentity>builder()
+                      .addAll(entry.getValue())
+                      .add(controller.getIdentity())
+                      .build();
+                } else if (controller.getDpad().isLeft() || controller.getDpad().isRight()) {
+                  return entry.getValue().stream()
+                      .filter(a -> a.equals(controller.getIdentity()))
+                      .collect(Collectors.toSet());
+                }
+                return entry.getValue();
+              }));
+    };
+  }
 
   @Override
-  public Publisher<Frame<I>> apply(final Publisher<T> tPublisher) {
-    return Flux.from(tPublisher)
-        .map(c -> c.getControllerSet().stream()
-            .reduce(c.getControllerMapping(),
-                combiner,
-                (a, b) -> Stream.of(a.entrySet(), b.entrySet())
-                    .flatMap(Collection::stream)
-                    .flatMap(entry -> entry.getValue().stream().map(controller -> new SimpleImmutableEntry<>(entry.getKey(), controller)))
-                    .collect(groupingBy(Entry::getKey,
-                        mapping(Entry::getValue, toSet())))))
-        .map(Frame::new);
+  public Publisher<Frame<C, I>> apply(final Publisher<S> setPublisher) {
+    return Flux.combineLatest(setPublisher,
+        Flux.from(setPublisher)
+          .map(c -> c.getControllerSet().stream()
+              .reduce(currentMapping.get(),
+                  combiner,
+                  (a, b) -> Stream.of(a.entrySet(), b.entrySet())
+                      .flatMap(Collection::stream)
+                      .flatMap(entry -> entry.getValue().stream().map(controller -> new SimpleImmutableEntry<>(entry.getKey(), controller)))
+                      .collect(groupingBy(Entry::getKey,
+                          mapping(Entry::getValue, toSet())))))
+          .doOnNext(currentMapping::set) // update currentMap to contain moved controllers
+        , Frame::new);
   }
 
   @Value
-  public static class Frame<I extends Identity> implements Controller.MapSupplier<I> {
+  public static class Frame<C extends Controller & Dpad.Supplier, I extends Identity> implements Controller.SetSupplier<C>, Controller.MapSupplier<I> {
+    @Delegate
+    private final Controller.SetSupplier<C> setSupplier;
     private final Map<I, Set<ControllerIdentity>> controllerMapping;
   }
 }
