92863cd85dd2f7cb861946fb7683b0253de7457a
Jeroen de Jong <thumbnail95@gmail.com>
2018-02-09 08:54:39 +0100

    Use TopicProcessor for keeping changes in assignment

--- src/main/java/io/leonis/subra/ipc/peripheral/ConfigurableMappingDeducer.java
+++ src/main/java/io/leonis/subra/ipc/peripheral/ConfigurableMappingDeducer.java
@@ -13,8 +13,9 @@ import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.BiFunction;
 import java.util.stream.Stream;
 import lombok.Value;
+import lombok.experimental.Delegate;
 import org.reactivestreams.Publisher;
-import reactor.core.publisher.Flux;
+import reactor.core.publisher.*;
 
 /**
  * @author jeroen.dejong.
@@ -26,31 +27,42 @@ public class ConfigurableMappingDeducer<
     S extends Controller.SetSupplier<C>>
 implements Deducer<S, Controller.MapSupplier<I>> {
 
-  private final AtomicReference<MapSupplier<I>> currentMapping =
-      new AtomicReference<>(Collections::emptyMap);
   private final BiFunction<Map<I, Set<ControllerIdentity>>, C, Map<I, Set<ControllerIdentity>>> combiner;
+  private final TopicProcessor<MapSupplier<I>> topic;
 
   public ConfigurableMappingDeducer(
-      final Publisher<Controller.MapSupplier<I>> mapSource,
-      final BiFunction<Map<I, Set<ControllerIdentity>>, C, Map<I, Set<ControllerIdentity>>> combiner
+    final Publisher<Controller.MapSupplier<I>> mapPublisher,
+    final BiFunction<Map<I, Set<ControllerIdentity>>, C, Map<I, Set<ControllerIdentity>>> combiner
   ) {
-    // Override current mapping with whatever comes through the publisher.
-    Flux.from(mapSource).subscribe(currentMapping::set);
     this.combiner = combiner;
+    this.topic = TopicProcessor.create();
+    // publish latest map to topic
+    Flux.from(mapPublisher).subscribe(topic::onNext);
   }
 
   @Override
   public Publisher<MapSupplier<I>> apply(final Publisher<S> setPublisher) {
-    return Flux.from(setPublisher)
+    return Flux.combineLatest(setPublisher, topic.distinctUntilChanged(MapSupplier::getControllerMapping), KnoepTroep::new)
           .map(c -> c.getControllerSet().stream()
-              .reduce(currentMapping.get().getControllerMapping(),
+              .reduce(c.getControllerMapping(),
                   combiner,
                   (a, b) -> Stream.of(a.entrySet(), b.entrySet())
                       .flatMap(Collection::stream)
-                      .flatMap(entry -> entry.getValue().stream().map(controller -> new SimpleImmutableEntry<>(entry.getKey(), controller)))
+                      .flatMap(entry -> entry.getValue().stream()
+                          .map(controller -> new SimpleImmutableEntry<>(entry.getKey(), controller)))
                       .collect(groupingBy(Entry::getKey,
                           mapping(Entry::getValue, toSet())))))
-          .<MapSupplier<I>>map(set -> () -> set) // wrap the result in a Controller.MapSupplier
-          .doOnNext(currentMapping::set); // update currentMap to contain moved controllers;
+          .<MapSupplier<I>>map(map -> () -> map)
+          .doOnNext(topic::onNext);
   }
+
+  @Value
+  private static class KnoepTroep<I extends Identity, C extends Controller>
+      implements Controller.MapSupplier<I>, Controller.SetSupplier<C> {
+    @Delegate
+    private final SetSupplier<C> setSupplier;
+    @Delegate
+    private final MapSupplier<I> mapSupplier;
+  }
+
 }
