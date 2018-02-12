df1e3f6f2f16267826aac1d80c9b552324d124d9
Jeroen de Jong <thumbnail@users.noreply.github.com>
2018-02-12 16:51:06 +0100

    Add javadoc to WrapperPacketSupplier

--- src/main/java/io/leonis/subra/game/data/WrapperPacketSupplier.java
+++ src/main/java/io/leonis/subra/game/data/WrapperPacketSupplier.java
@@ -3,8 +3,16 @@ package io.leonis.subra.game.data;
 import org.robocup.ssl.Wrapper.WrapperPacket;
 
 /**
- * @author jeroen.dejong.
+ * The Interface WrapperPacketSupplier.
+ *
+ * Contains a {@link WrapperPacket}.
+ * 
+ * @author Jeroen de Jong
  */
 public interface WrapperPacketSupplier {
+
+  /**
+   * @return The robocup ssl wrapper packet. 
+   */
   WrapperPacket getWrapperPacket();
 }
